5988a1091b3ef75839712d1a94856a41080dc006
Kha50 <156250419+Kha50@users.noreply.github.com>
2026-05-27 22:48:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+require([
+  "esri/WebMap",
+  "esri/views/MapView",
+  "esri/widgets/LayerList",
+  "esri/widgets/Legend"
+], function(WebMap, MapView, LayerList, Legend) {
 
+  const webmap = new WebMap({
+    portalItem: {
+      id: "306f6ce39c6a46f880cba103bec47daf"
+    }
+  });
+
+  const view = new MapView({
+    container: "viewDiv",
+    map: webmap
+  });
+
+  const layerList = new LayerList({
+    view: view
+  });
+
+  const legend = new Legend({
+    view: view
+  });
+
+  view.ui.add(layerList, "top-right");
+  view.ui.add(legend, "bottom-right");
+});
